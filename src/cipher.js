f62100c5bf9287e2d952ceec7490ff74109ef513
MargotRivera <margotimeldarb@gmail.com>
2018-05-25 16:00:26 -0500

    Cifrado Cesar Avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,23 @@
+function enter(){
+  var password = document.getElementById('password').value;
+ if(password == "felizcumple"){
+    var entrar = document.getElementById('container');
+    entrar.style.display = 'block';
+    var root = document.getElementById('root');
+    root.style.display = 'none';
+    }
+  }
+
+function mycipher(){
+    var text = document.getElementById('texto1').value;
+    var letter = text.charArt(0);
+    console.log(letter);
+  }
+
+
+
 window.cipher = {
-  // ... 
-};
+encode : mycipher,
+decode : decipher,
+entrar: enter
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const cifrar = document.getElementById('cipher');
+cifrar.addEventListener('click',window.cipher.encode);
+const entrar = document.getElementById('enter');
+entrar.addEventListener('click',window.cipher.entrar); 
